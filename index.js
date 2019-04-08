7c87da9c637d2bcdc91ce34ea5c4db439d16f347
Eslam-Tealib <47720115+Eslam-Tealib@users.noreply.github.com>
2019-04-08 03:54:11 +0200

    Removed some unnecessary files.
    Removed some images and added emojis instead and made some minor changes in the code.

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-var randomNumber1 = Math.random();
-randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
-document.querySelector(".first-player img").setAttribute("src", "images/" + randomNumber1 + ".png");
+  var randomNumber1 = Math.random();
+  randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
+  document.querySelector(".first-player img").setAttribute("src", "images/" + randomNumber1 + ".png");
 
-var randomNumber2 = Math.random();
-randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
-document.querySelector(".second-player img").setAttribute("src", "images/" + randomNumber2 + ".png");
+  var randomNumber2 = Math.random();
+  randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
+  document.querySelector(".second-player img").setAttribute("src", "images/" + randomNumber2 + ".png");
 
-if(randomNumber1 === randomNumber2){
-    document.querySelector("h1").innerHTML = "Draw!";
-}
-else if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "<img src='images/flag.png' alt=''> Player 1 Wins";
-}
-else if(randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "Player 2 Wins <img src='images/flag.png' alt=''>";
-}
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Draw! 🚩";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+  }
